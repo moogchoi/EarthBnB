7be2ecb43b8f32917b50ed06ba1d17b835fa01da
Mugil <mugil.choi@gmail.com>
2023-07-13 10:14:00 -0400

    WIP

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -37,7 +37,50 @@ router.get('/current', requireAuth, async (req, res) => {
   res.json({ Spots: spotsWithAvgRating });
 })
 
+// get spot details from id
+router.get('/:spotId', async (req, res) => {
+  const foundSpot = await Spot.findOne({
+    where: {
+      id: req.params.spotId
+    },
+    include: [
+      {
+        model: User,
+        as: "Owner",
+        attributes: ["id", 'firstName', "lastName"]
+    }
+    ]
+  })
+  if (!foundSpot) {
+    res.status(404)
+    return res.json({
+      "message": "Spot couldn't be found"
+    })
+  }
 
+  const spotReviews = await foundSpot.getReviews()
+  foundSpot.dataValues.numReviews = spotReviews.length
+
+  const avgRatingResult = await Review.findOne({
+    attributes: [
+      [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']
+    ],
+    where: { spotId: foundSpot.id }
+  });
+  const avgStarRating = avgRatingResult ? avgRatingResult.dataValues.avgRating : null;
+  foundSpot.dataValues.avgStarRating = avgStarRating
+
+  const spotImages = await SpotImage.findAll(
+    {
+    where: {
+        spotId: foundSpot.id
+    }
+  })
+
+  foundSpot.dataValues.SpotImages = spotImages
+
+  res.json(foundSpot)
+})
 
 // get all spots
 router.get('/', async (req, res) => {
