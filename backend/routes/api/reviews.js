08ca46613317c94271df3a04812246dfa6bad0ad
Mugil <mugil.choi@gmail.com>
2023-07-14 10:56:30 -0400

    fixed format of response

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -18,23 +18,69 @@ router.get('/current', requireAuth, async (req, res) => {
         attributes: ["id", 'firstName', "lastName"]
       },
       {
-        model:Spot,
+        model: Spot,
         include: [
-            {
-                model: SpotImage,
-                as: 'previewImage',
-                attributes: ['url']
-            }
+          {
+            model: SpotImage,
+            as: 'previewImage',
+            attributes: ['url']
+          }
         ],
-        attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price' ]
+        attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price']
       },
       {
         model: ReviewImage,
         attributes: ["id", 'url']
       },
+    ],
+    attributes: [
+      'id',
+      'userId',
+      'spotId',
+      'review',
+      'stars',
+      'createdAt',
+      'updatedAt'
     ]
-  })
-  res.json({ Reviews: allReviews})
-})
+  });
+
+  const formattedReviews = allReviews.map(review => {
+    return {
+      id: review.id,
+      userId: review.userId,
+      spotId: review.spotId,
+      review: review.review,
+      stars: review.stars,
+      createdAt: review.createdAt,
+      updatedAt: review.updatedAt,
+      User: {
+        id: review.User.id,
+        firstName: review.User.firstName,
+        lastName: review.User.lastName
+      },
+      Spot: {
+        id: review.Spot.id,
+        ownerId: review.Spot.ownerId,
+        address: review.Spot.address,
+        city: review.Spot.city,
+        state: review.Spot.state,
+        country: review.Spot.country,
+        lat: review.Spot.lat,
+        lng: review.Spot.lng,
+        name: review.Spot.name,
+        price: review.Spot.price,
+        previewImage: review.Spot.previewImage.length > 0 ? review.Spot.previewImage[0].url : null
+      },
+      ReviewImages: review.ReviewImages.map(image => {
+        return {
+          id: image.id,
+          url: image.url
+        };
+      })
+    };
+  });
+
+  res.json({ Reviews: formattedReviews });
+});
 
 module.exports = router;
