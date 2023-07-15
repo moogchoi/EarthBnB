78d256b143eff42b35a9fef33134180808542f56
Mugil <mugil.choi@gmail.com>
2023-07-15 07:29:50 -0400

    finished delete, edit, and add image

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -83,5 +83,88 @@ router.get('/current', requireAuth, async (req, res) => {
   res.json({ Reviews: formattedReviews });
 });
 
+// add an image to a review based on the review's id
+router.post('/:reviewId/images', requireAuth, async (req, res) => {
+  const review = await Review.findByPk(req.params.reviewId);
+
+  if (!review) {
+    res.status(404);
+    res.json({
+      "message": "Review couldn't be found"
+    })
+  }
+  if (review.userId !== req.user.id) {
+    res.status(403);
+    res.json({
+      "message": "Review must belong to the current user"
+    })
+  }
+  const reviewImageCount = await ReviewImage.count({
+    where: { reviewId: req.params.reviewId}
+  })
+  if (reviewImageCount > 10){
+    res.status(403);
+    res.json({
+      "message": "Maximum number of images for this resource was reached"
+    })
+  }
+  const { url } = req.body;
+  const newImage = await ReviewImage.create({reviewId: req.params.reviewId, url})
+  return res.status(201).json(newImage)
+})
+
+// review validator
+const reviewValidator = [
+  check('review')
+      .exists({ checkFalsy: true })
+      .withMessage('Review text is required'),
+  check('stars')
+      .exists({ checkFalsy: true })
+      .isInt({ min: 1, max: 5 })
+      .withMessage('Stars must be an integer from 1 to 5'),
+  handleValidationErrors
+]
+
+// edit a review
+router.put('/:reviewId', reviewValidator, requireAuth, async (req, res) => {
+  const review = await Review.findByPk(req.params.reviewId);
+  if(!review){
+    return res.status(404).json({"message": "Review couldn't be found"})
+  }
+
+  if (review.userId !== req.user.id) {
+    res.status(403);
+    res.json({
+      "message": "Review must belong to the current user"
+    })
+  }
+
+  const updateReview = await review.update({
+    review: req.body.review,
+    stars: req.body.stars
+  })
+  res.json(updateReview)
+});
+
+// delete a review
+router.delete('/:reviewId', requireAuth, async (req, res, next) => {
+  const review = await Review.findByPk(req.params.reviewId);
+
+  if(!review){
+    return res.status(404).json({"message": "Review couldn't be found"})
+  }
+
+  if (review.userId !== req.user.id) {
+    res.status(403);
+    res.json({
+      "message": "Review must belong to the current user"
+    })
+  }
+
+  await review.destroy()
+  res.json({
+    "message": "Successfully deleted"
+  })
+});
 
 module.exports = router;
